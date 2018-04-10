56662cbb63f9f27243c4617756b0a54d9c8f0410
Ramon de Souza Almeida <ramonsouza.92@hotmail.com>
2018-04-10 10:11:26 -0300

    banco com chave estrangeira

--- designpattern.sql
+++ designpattern.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Abr-2018 às 01:32
+-- Generation Time: 05-Abr-2018 às 03:42
 -- Versão do servidor: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -38,28 +38,7 @@ CREATE TABLE `contato` (
 
 INSERT INTO `contato` (`id`, `nome`, `email`) VALUES
 (1, 'Ramon', 'ramon@gmail.com'),
-(10, 'Victor', 'victor@gmail.com'),
-(7, 'Kaio', 'kaio@msn.com'),
-(8, 'Tião', 'tião@gmail.com');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `departamento`
---
-
-CREATE TABLE `departamento` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `departamento`
---
-
-INSERT INTO `departamento` (`id`, `nome`) VALUES
-(1, 'RH'),
-(2, 'Secretaria');
+(7, 'Kaio', 'kaio@msn.com');
 
 --
 -- Indexes for dumped tables
@@ -72,12 +51,6 @@ ALTER TABLE `contato`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `departamento`
---
-ALTER TABLE `departamento`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -85,12 +58,7 @@ ALTER TABLE `departamento`
 -- AUTO_INCREMENT for table `contato`
 --
 ALTER TABLE `contato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT for table `departamento`
---
-ALTER TABLE `departamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
